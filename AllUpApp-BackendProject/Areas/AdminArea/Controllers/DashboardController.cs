9d651817e534f60a072bdfc5aeb6042dd9a45293
SabirMehdiyev <sabirsm@code.edu.az>
2023-10-27 23:51:16 +0400

    add Admin dashboard

---
+++ AllUpApp-BackendProject/Areas/AdminArea/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AllUpApp_BackendProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
